8ee77dee35f7d12d19f029e8e6c57e438891f2f6
Nadia Montalbán <nadia.alumno26@gmail.com>
2024-12-01 20:58:40 +0000

    Just changed a couple of things, nothing major

--- models/data.marts/core/dim_users.sql
+++ models/data.marts/core/dim_users.sql
@@ -10,10 +10,12 @@ dim_users AS (
         , first_name
         , last_name
         , email
+        , coalesce (regexp_like(email, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$')= true,false) as is_valid_email_address
         , phone_number
         , created_at_utc
         , updated_at_utc
-        , date_load
+        , if_deleted
+        , date_load_utc
     FROM stg_sql_server_dbo__users
     )
 
